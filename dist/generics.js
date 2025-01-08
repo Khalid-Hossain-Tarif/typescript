35c30ab87f706b6bc95f7f2f380ca3184406b097
Khalid-Hossain-Tarif <khalidhossain1311@gmail.com>
2025-01-08 17:20:23 +0600

    update: generics

--- dist/generics.js
+++ dist/generics.js
@@ -1,4 +1,20 @@
 "use strict";
-let myAge = 30;
-console.log(myAge);
+class KeyValuePair {
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+let pair = new KeyValuePair('1', 'Generic Classes');
+let pairTwo = new KeyValuePair(1, 'Generic Classes 2');
+pair.value.concat('-');
+console.log(pair);
+console.log(pairTwo);
+class ArrayUtils {
+    static wrapInArray(value) {
+        return value;
+    }
+}
+let arrays = ArrayUtils.wrapInArray(1);
+console.log(arrays);
 //# sourceMappingURL=generics.js.map
--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,21 @@
-let myAge: number = 30;
-console.log(myAge)
- 
+//Generic classes
+class KeyValuePair<K, V> {
+    constructor(public key: K, public value: V) {}
+}
+
+let pair =  new KeyValuePair('1', 'Generic Classes')
+let pairTwo =  new KeyValuePair<number, string>(1, 'Generic Classes 2') //can declare types like <number, string>
+pair.value.concat('-')
+console.log(pair)
+console.log(pairTwo)
+
+
+//Generic functions
+class ArrayUtils {
+    static wrapInArray<T>(value: T) {
+        return value
+    }
+}
+
+let arrays = ArrayUtils.wrapInArray(1)  
+console.log(arrays)
